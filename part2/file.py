781aa6179e4c0bdf21c5ea9f4460cef84e660de4
PeacefulPolymath <lokajna2012@gmail.com>
2026-08-03 21:23:28 +0530

    Added part2

---
+++ part2/file.py
@@ -0,0 +1,54 @@
+firstname = 'Peaceful'
+lastname = 'Polymath'
+age = 14
+color = 'Red'
+print('I am ' + firstname + ' ' + lastname + ' .I am ' + str(age) + ' years old and I love the colour ' + color)
+
+# Challenge Spot Error
+# Code A
+'''
+x = 10
+if x > 5:
+    print("Big")
+    print("Number")
+
+# Code B
+y = 3
+    print(y)
+
+
+# Code C
+if y < 5:
+print("Small")
+'''
+
+#Correct Code
+'''
+x = 10
+if x > 5:
+    print("Big")
+    print("Number")
+
+# Code B
+y = 3
+print(y)
+
+# Code C
+if y < 5:
+print("Small")
+'''
+
+
+#Challenge
+num1 = 2
+num2 = 3
+sum = num1 + num2
+product = num1 * num2
+print('The sum is ' + str(sum))
+print('The product is ' + str(product))
+
+#Challenge
+name = 'Peaceful Polymath'
+city = 'Vizag'
+food = 'Potato Curry'
+print('I am ' + name + ", I currently live in " + city +" . I love to eat " + food)
