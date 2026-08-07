4e61d54b380cd8aed4993b2fb731b73cd4a5d86f
PeacefulPolymath <lokajna2012@gmail.com>
2026-08-07 20:21:10 +0530

    Added 13.py

---
+++ PART1/13.py
@@ -0,0 +1,6 @@
+celsius = float(input('Enter the temperature in Celsius: '))
+fahrenheit = (celsius * (9/5)) + 32
+print('The given Celsius in fahrenheit is' , fahrenheit)
+difference = celsius - fahrenheit
+absolute_difference = abs(difference)
+print('The absolute difference between the Celsius and Fahrenheit is' , absolute_difference)
