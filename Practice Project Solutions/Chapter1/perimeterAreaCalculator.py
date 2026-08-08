12b784828df2afa275c35b73268dde002b93c3b5
PeacefulPolymath <lokajna2012@gmail.com>
2026-08-08 20:22:48 +0530

    Added perimeterAreaCalculator.py

---
+++ Practice Project Solutions/Chapter1/perimeterAreaCalculator.py
@@ -0,0 +1,6 @@
+length = int(input('Enter the length of the rectangle (Only numbers):\n'))
+width = int(input('Enter the width of the rectangle (Only numbers):\n'))
+area = length * width
+perimeter = 2*(length + width)
+print(f'Area of the Rectangle:\n{area}')
+print(f'Perimeter of Rectangle:\n{perimeter}')
