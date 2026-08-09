edf0e07940d5e86a446638d22b6028415fe672f9
PeacefulPolymath <lokajna2012@gmail.com>
2026-08-09 19:15:21 +0530

    Saving my local terminal changes

--- PART1/14.py
+++
@@ -1,7 +0,0 @@
-word = input('Enter the word you desire: ')
-censor_multiplier = int(input('Enter the censor multiplier number: '))
-length = len(word)
-squarelength = (length ** 2) + censor_multiplier
-censorship_block = squarelength * '#'
-print(word)
-print(censorship_block)
