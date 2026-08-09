9dbd349d4d199fdfacfcd90c5dba861e7b85c353
PeacefulPolymath <lokajna2012@gmail.com>
2026-08-09 18:34:06 +0530

    Delete PART1 directory

--- PART1/12.py
+++
@@ -1,9 +0,0 @@
-minutes = int(input('Enter the number of minutes: '))
-minperday = 60 * 24
-minperhour = 60
-days = minutes // minperday
-minafterdays = minutes % minperday
-hours = minafterdays // minperhour
-minafterhours = minafterdays % minperhour
-
-print(days , 'Days' , hours , 'Hours' , minafterhours , 'minutes')
