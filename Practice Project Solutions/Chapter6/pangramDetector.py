fdf31afbd003e80ac9c9fa765a2498051aa2a174
PeacefulPolymath <lokajna2012@gmail.com>
2026-08-11 12:07:39 +0530

    added pangramDetector.py

---
+++ Practice Project Solutions/Chapter6/pangramDetector.py
@@ -0,0 +1,13 @@
+def is_pangram(sentence):
+    alphas = list("abcdefghijklmnopqrstuvwxyz")
+    occurs = []
+    for i in sentence:
+        if i in alphas:
+            if i not in occurs:
+                occurs.append(i)
+    if len(occurs) == 26:
+        return 'This sentence is a pangram'
+    else:
+        return 'This sentence is not a pangram'
+#print(is_pangram('Hello, world!'))
+        
