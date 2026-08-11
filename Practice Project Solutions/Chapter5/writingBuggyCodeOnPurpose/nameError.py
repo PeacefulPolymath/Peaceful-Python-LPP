327efc49a2cd7ce7f3bd559596dc77755d48a047
PeacefulPolymath <lokajna2012@gmail.com>
2026-08-11 11:23:36 +0530

    added nameError.py

---
+++ Practice Project Solutions/Chapter5/writingBuggyCodeOnPurpose/nameError.py
@@ -0,0 +1,4 @@
+calls = int(input('Enter the toal number of calls you receives this week: '))
+spamcalls = int(input('Enter the total number of spam calls out of those calls; '))
+normal = calls - spam
+print('You got' , normal, 'normal calls.')
