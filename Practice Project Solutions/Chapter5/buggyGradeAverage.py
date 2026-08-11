3b963ded5018103d59d9b7d814aaef465d1e94b7
PeacefulPolymath <lokajna2012@gmail.com>
2026-08-11 10:59:38 +0530

    added buggyGradeAverage.py

--- Practice Project Solutions/Chapter5/buggyGradeAverage.py
+++ Practice Project Solutions/Chapter5/buggyGradeAverage.py
@@ -16,3 +16,21 @@ while True:
 avg = calculate_grade_average(counter, total)
 print('The grade average is:', avg)
 '''
+
+#Fixed Code
+def calculate_grade_average(grade_sum, number_of_grades):
+    grade_average = grade_sum / number_of_grades
+    return grade_average
+
+counter = 0
+total = 0
+while True:
+    print('Enter a grade, or "done" if done entering grades:')
+    grade = input()
+    if grade == 'done':
+        break
+    counter = counter + 1
+    total = total + int(grade)
+
+avg = calculate_grade_average(total , counter)
+print('The grade average is:', avg)
